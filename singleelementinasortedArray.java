6321df317c7546f09736db697f073ba329264293
Abhinandan Onajol <120156124+abhinandan976@users.noreply.github.com>
2025-08-25 23:22:16 +0530

    Add files via upload

---
+++ singleelementinasortedArray.java
@@ -0,0 +1,27 @@
+public class singleelementinasortedArray {
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,3};
+        int result = singleNonDuplicate(arr);
+        System.out.println("The single element in the array is:" + result);
+    }
+
+    static int singleNonDuplicate(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while(low < high) {
+            int mid = low + (high - low) / 2;
+            
+            if(mid % 2 == 1) {
+                mid--;
+            }
+            if(nums[mid] == nums[mid + 1]) {
+                low = mid + 2;
+            }
+            else {
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+} 
