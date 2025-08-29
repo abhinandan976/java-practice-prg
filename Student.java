a4429fbb08d9b84f9223b49a3f84b3391a665bc1
Abhinandan Onajol <120156124+abhinandan976@users.noreply.github.com>
2025-08-29 14:42:35 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -2,13 +2,25 @@ public class Student {
     String name;
     int rollno;
 
-    public Student(String name, int rollno) {
+    private Student(String name, int rollno) {
         this.name = name;
         this.rollno = rollno;
     }
 
+    public static Student createStudent(String name, int rollno) {
+        return new Student(name, rollno);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+ 
     public static void main(String[] args) {
-        Student s1 = new Student("Pinki",101);
+        Student s1 = Student.createStudent("Pinki",101);
         System.out.println(s1.name + " " + s1.rollno);
     }
 }
